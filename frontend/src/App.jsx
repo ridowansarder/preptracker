c55f0dab4e65626eac7c18c81009bddbdb4a3110
Ridowan Sarder <ridwanzuraiz.gmail.com>
2026-03-28 13:48:51 +0600

    app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
